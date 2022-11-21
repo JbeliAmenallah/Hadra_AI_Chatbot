77f97103088f8c1622cbba531e62e697f820c73d
Amen <70348504+JbeliAmenallah@users.noreply.github.com>
2022-11-21 20:17:23 +0100

    Add files via upload

---
+++ response.js
@@ -0,0 +1,24 @@
+
+const responseObj = {
+  
+  Hi:"Hey ! How are you doing ?",
+  Is_anyone_there:"Hey ! How are you doing ?",
+  Hey:"Hey ! How are you doing ?",
+  Hola:"Hey ! How are you doing ?",
+  Hello:"Hey ! How are you doing ?",
+  Hey: "Hey ! How are you doing ?",
+  Hello: "Hey ! How are you doing ?",
+  hello: "Hey ! How are you doing ?",
+  hey: "Hey! What's Up",
+  today: new Date().toDateString(),
+  time: new Date().toLocaleTimeString(),
+  "Are you a robot":"Yes I am a robot, but I’m a good one. Let me prove it. How can I help you?",
+  "Good bye":"Have a nice day",
+  Bye:"See you!",
+  "See you later":"See you!",
+  Goodbye:"See you!", 
+  "Nice chatting to you, bye":"Bye! Come back again soon.",
+  "Till next time":"Bye! Come back again soon.",
+  
+
+};
